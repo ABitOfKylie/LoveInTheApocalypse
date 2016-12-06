42cd743bc37e6333bcb38005dbcda43d7f2e8430
Kylie Soderblom <MacDream@Kylies-MacBook-Pro.local>
2016-12-05 22:04:06 -0800

    added zombiescript question/resp 'outline'

---
+++ src/models/zombieScript Taylor.js
@@ -0,0 +1,126 @@
+module.exports = {
+
+	{name: "zSwamp",
+	description: "He's falling apart but he still has room to love",
+	image: "url",
+	locationName: "Swamp",
+	locationImage: "url",
+	questions: [
+		{
+			question: "How are you today?",
+			responses: [
+				{response: "Great", value: 10},
+				{response: "None of your business", value: -20}
+			]
+		},
+		{
+			question: "How do you feel about zombies?",
+			responses: [
+				{response: "I hate them", value: 10},
+				{response: "They're awesome!", value: -20}
+			]	
+		},
+		
+		{
+			question: "How do you feel about zombies?",
+			responses: [
+				{response: "I hate them", value: 10},
+				{response: "They're awesome!", value: -20}
+			]	
+		}
+
+	]
+	},
+	{	name: "zBeach",
+	description: "He's falling apart but he still has room to love",
+	image: "url",
+	locationName: "Beach",
+	locationImage: "url",
+	questions: [
+		{
+			question: "How are you today?",
+			responses: [
+				{response: "Great", value: 10},
+				{response: "None of your business", value: -20}
+			]
+		},
+		{
+			question: "How do you feel about zombies?",
+			responses: [
+				{response: "I hate them", value: 10},
+				{response: "They're awesome!", value: -20}
+			]	
+		},
+		
+		{
+			question: "How do you feel about zombies?",
+			responses: [
+				{response: "I hate them", value: 10},
+				{response: "They're awesome!", value: -20}
+			]	
+		}
+
+	]},
+	{
+	name: "zSnow",
+	description: "He's falling apart but he still has room to love",
+	image: "url",
+	locationName: "Snow",
+	locationImage: "url",
+	questions: [
+		{
+			question: "How are you today?",
+			responses: [
+				{response: "Great", value: 10},
+				{response: "None of your business", value: -20}
+			]
+		},
+		{
+			question: "How do you feel about zombies?",
+			responses: [
+				{response: "I hate them", value: 10},
+				{response: "They're awesome!", value: -20}
+			]	
+		},
+		
+		{
+			question: "How do you feel about zombies?",
+			responses: [
+				{response: "I hate them", value: 10},
+				{response: "They're awesome!", value: -20}
+			]	
+		}
+
+	]
+	},
+	{name: "zMansion",
+	description: "He's falling apart but he still has room to love",
+	image: "url",
+	locationName: "Mansion",
+	locationImage: "url",
+	questions: [
+		{
+			question: "How are you today?",
+			responses: [
+				{response: "Great", value: 10},
+				{response: "None of your business", value: -20}
+			]
+		},
+		{
+			question: "How do you feel about zombies?",
+			responses: [
+				{response: "I hate them", value: 10},
+				{response: "They're awesome!", value: -20}
+			]	
+		},
+		
+		{
+			question: "How do you feel about zombies?",
+			responses: [
+				{response: "I hate them", value: 10},
+				{response: "They're awesome!", value: -20}
+			]	
+		}
+
+	]}
+}
